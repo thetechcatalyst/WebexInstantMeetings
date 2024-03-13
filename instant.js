1fce31e16bf785d43d3fea171f02c30dfc08b272
technologyordie <adam@fullspansolutions.com>
2024-03-13 17:24:58 -0400

    Added comment to code

--- instant.js
+++ instant.js
@@ -2,6 +2,6 @@ import xapi from 'xapi';
 
 xapi.event.on('UserInterface Extensions Panel Clicked', (event) => {
     if(event.PanelId === "WEBEX"){
-      xapi.Command.Webex.Meetings.InstantMeeting.Start();
+      xapi.Command.Webex.Meetings.InstantMeeting.Start();  //requires instant meetings to be enabled on the device from Control Hub
     }
 });
